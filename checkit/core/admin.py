6293aa0c62eb711e151f6dd03de6003a0bc15216
Ulrich Matchi Aivodji <umaivodj@laas.fr>
2014-08-08 01:59:44 +0200

    add profile in the admin dashboard

--- checkit/core/admin.py
+++ checkit/core/admin.py
@@ -1,4 +1,6 @@
 from checkit.feeds.models import Feed, CheckTag
+from checkit.auth.models import Profile
 from django.contrib import admin
 admin.site.register(Feed)
 admin.site.register(CheckTag)
+admin.site.register(Profile)
